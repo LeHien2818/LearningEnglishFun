61605f3cc94dd837d1456b2c3bb868fc6e130a2d
LeHien2818 <hienltv281823@gmail.com>
2023-10-04 09:16:09 +0700

    set up translate view

--- src/main/java/englishlearningapp/englearning/API_Connection/TranslateAPIConnection.java
+++ src/main/java/englishlearningapp/englearning/API_Connection/TranslateAPIConnection.java
@@ -7,7 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class APIConnection {
+public class TranslateAPIConnection {
     public static HttpURLConnection getTranslateApiCon (String langFrom, String langTo, String text) throws IOException {
         String urlString = "https://script.google.com/macros/s/AKfycbxC0KJMUCQPJr05hz-oBxixClQJ59vL0BYPvih3-ed8fTRfU2pxuUIu6K4slOtSYf68/exec"
                 + "?q="
@@ -22,7 +22,6 @@ public class APIConnection {
     }
     public static String translateText(String langFrom, String langTo, String text) throws IOException {
         HttpURLConnection connection = getTranslateApiCon(langFrom, langTo, text);
-        //connection.setRequestProperty("User-Agent", "Mozilla/5.0");
         BufferedReader input =new BufferedReader(new InputStreamReader(connection.getInputStream()));
         StringBuilder reponse =new StringBuilder();
         String line;
