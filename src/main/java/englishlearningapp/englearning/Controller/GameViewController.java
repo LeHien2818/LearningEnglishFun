76c7ff856fcd0870dac9c8a229439633c36cb1e2
Lee-412 <115365338+Lee-412@users.noreply.github.com>
2023-10-11 14:12:41 +0700

    update
    update vocab, game code

--- src/main/java/englishlearningapp/englearning/Controller/GameViewController.java
+++ src/main/java/englishlearningapp/englearning/Controller/GameViewController.java
@@ -1,16 +1,93 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package englishlearningapp.englearning.Controller;
 
-import javafx.event.ActionEvent;
 import java.io.IOException;
+import java.util.Random;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class GameViewController {
-    public GameViewController() throws IOException {
+    @FXML
+    private TextArea Scoregame = new TextArea();
+    private int score = 0;
+    private int quesnumber = 0;
+    Random random = new Random();
+    private int randomIndex;
+
+    public GameViewController() {
     }
 
-    public void clickSearch (ActionEvent event) throws IOException {
+    public int getQuesNumber() {
+        return this.quesnumber;
+    }
+
+    public void setQuesNumber(int quesnumber) {
+        this.quesnumber = quesnumber;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public String toString(int score) {
+        return "[" + score + "]";
+    }
+
+    public void setTextScore(String s) {
+        this.Scoregame.setText(s);
+    }
+
+    public int getRandom() {
+        return this.randomIndex;
+    }
+
+    public void setRandom(int size) {
+        this.randomIndex = this.random.nextInt(size - 1) + 1;
+    }
+
+    public void startGame() {
+    }
+
+    public void endGame(ActionEvent event, String s) throws IOException {
+        if (s.equals("vocab") || s.equals("grammar") || s.equals("connect")) {
+            SceneController.switchScene(event, SceneController.gameRoot);
+            this.setScore(0);
+            this.setQuesNumber(0);
+            this.setTextScore(this.toString(this.getScore()));
+        }
+
+    }
+
+    public void clickSearch(ActionEvent event) throws IOException {
         SceneController.switchScene(event, SceneController.searchRoot);
     }
-    public void clickTranslate (ActionEvent event) throws IOException {
+
+    public void clickTranslate(ActionEvent event) throws IOException {
         SceneController.switchScene(event, SceneController.translateRoot);
+    }
+
+    public void clickGame(ActionEvent event) throws IOException {
+        SceneController.switchScene(event, SceneController.gameRoot);
+    }
+
+    public void clickVocab(ActionEvent event) throws IOException {
+        SceneController.switchScene(event, SceneController.vocabRoot);
+    }
+
+    public void clickConnect(ActionEvent event) throws IOException {
+        SceneController.switchScene(event, SceneController.connectRoot);
+    }
+
+    public void clickGrammar(ActionEvent event) throws IOException {
+        SceneController.switchScene(event, SceneController.grammarRoot);
     }
 }
